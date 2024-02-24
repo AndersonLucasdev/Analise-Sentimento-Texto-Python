c58799f97f9f66493b9d4c5c8b4db148c8a66455
anderson <andinlucas8@gmail.com>
2024-02-24 19:54:07 -0300

    feat/ Adicionando o dataset

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import numpy as np
 import os
 import sklearn as sk
+
+def carregar_dados(folder_path):
+    texts = []
+    labels = []
+    for label in ['pos', 'neg']:
+        folder = os.path.join(folder_path, label)
+        for filename in os.listdir(folder):
+            with open(os.path.join(folder, filename), 'r', encoding='utf-8') as file:
+                texts.append(file.read())
+            labels.append(label)
+    return texts, labels
