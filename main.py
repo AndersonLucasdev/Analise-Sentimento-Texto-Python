42bb024f8df825bb3e825d04db10a1c8aed3a858
anderson <andinlucas8@gmail.com>
2024-02-25 09:55:50 -0300

    feat/ Criando função para carregar os dados

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import numpy as np
 import os
 import sklearn as sk
 
-def carregar_dados(folder_path):
-    texts = []
-    labels = []
-    for label in ['pos', 'neg']:
-        folder = os.path.join(folder_path, label)
-        for filename in os.listdir(folder):
-            with open(os.path.join(folder, filename), 'r', encoding='utf-8') as file:
-                texts.append(file.read())
-            labels.append(label)
-    return texts, labels
+
+class SentimentAnalysis:
+    def __init__(self, train_folder, test_folder):
+        self.train_folder = train_folder
+        self.test_folder = test_folder
+
+    def load_data(self, folder_path):
+        texts = []
+        labels = []
+        for label in ['pos', 'neg']:
+            folder = os.path.join(folder_path, label)
+            for filename in os.listdir(folder):
+                with open(os.path.join(folder, filename), 'r', encoding='utf-8') as file:
+                    texts.append(file.read())
+                labels.append(label)
+        return texts, labels
